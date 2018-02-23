5c36cae34c3ffbe0ee928a367723caf3b6a94db4
thejordanprice <thejordanprice@gmx.com>
2018-02-23 17:55:10 -0500

    fixed mongoose warnings

--- app.js
+++ app.js
@@ -51,17 +51,6 @@ const passportConfig = require('./config/passport');
  * Create Express server.
  */
 const app = express();
-
-/**
- * Connect to MongoDB.
- */
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI);
-mongoose.connection.on('error', () => {
-  console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
-  process.exit();
-});
-
 /**
  * Express configuration.
  */
